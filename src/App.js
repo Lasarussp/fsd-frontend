269b9aa2af8c002462155f39cd03013a93a31402
Lasarussp <001lasarus@gmail.com>
2023-04-04 12:34:19 +0530

    given

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import CreateEmployee from './Components/CreateEmployee';
+import EmployeeList from './Components/EmployeeList';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import UpdateEmployee from './Components/UpdateEmployee';
 
 function App() {
   return (
+   <BrowserRouter>
     <div className="App">
-     
+      <Header />
+      <Routes>
+        <Route exact path='/'  element={<EmployeeList />}/>
+        <Route path='/employees/create' element={<CreateEmployee />} />
+        <Route path='/employees/:empID/update' element={<UpdateEmployee />} />
+      </Routes>
+      <Footer />
     </div>
+   
+   </BrowserRouter>
   );
 }
 
