eee6581ce6ac69a6dfd6378637b3a03479a7cd6c
Lasarussp <001lasarus@gmail.com>
2023-04-04 12:49:09 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter,Routes, Route } from 'react-router-dom';
-
 import './App.css';
 import CreateEmployee from './Components/CreateEmployee';
 import EmployeeList from './Components/EmployeeList';
